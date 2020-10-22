0b20bb6eaed2e4585550f644efeaa6db790a29ed
RemigiuszDrobinski <curiosisciekawski@gmail.com>
2020-10-22 10:06:34 +0200

    Add playerMovement and jumping

---
+++ Gobaith/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed, jumpForce, checkRadius;
+    private float moveInput;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+            
+        Debug.Log(isGrounded);
+    }
+}
