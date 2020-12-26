3509ecdaa6b2120f4bb1756cf434c3955b3f657f
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2020-12-26 21:55:18 +0100

    Add Monster_02

---
+++ Gobaith/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public Transform bulletPoint;
+    public GameObject bulletGO;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    
+    void Update()
+    {
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(bulletGO, bulletPoint.position, bulletPoint.rotation);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+            timeBtwShots -= Time.deltaTime;
+    }
+}
