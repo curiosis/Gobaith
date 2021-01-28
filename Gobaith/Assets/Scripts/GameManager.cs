e1129b193467201eb80ec5a4e227df65a8b4fb3d
KamilDonda <dondakamil@gmail.com>
2021-01-28 01:01:44 +0100

    Add names for desert maps

--- Gobaith/Assets/Scripts/GameManager.cs
+++ Gobaith/Assets/Scripts/GameManager.cs
@@ -10,11 +10,16 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        levelNames[0] = "Thickets [Ithali Forest]";
-        levelNames[1] = "Spiky gorge [Ithali Forest]";
-        levelNames[2] = "Pedestal [Ithali Forest]";
+        levelNames[0] = "Thickets";
+        levelNames[1] = "Spiky gorge";
+        levelNames[2] = "Pedestal";
         levelNames[3] = "Mountain of revelation";
-        levelNames[4] = "Cave de Bélly [Ithali Forest]";
+        levelNames[4] = "Cave de Bélly";
+	levelNames[5] = "Tany efitra";
+	levelNames[6] = "Lohasahan'i Misty";
+	levelNames[7] = "Grotta tar-ramel";
+	levelNames[8] = "Havoana rahona";
+	levelNames[9] = "Tany manidina";
 
         apples[0] = 1;
     }
