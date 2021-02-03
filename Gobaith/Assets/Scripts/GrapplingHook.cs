3564b1c9ecca01aca926a0b92011c4066201fa64
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-02-03 10:37:17 +0100

    Update grappling hook script

--- Gobaith/Assets/Scripts/GrapplingHook.cs
+++ Gobaith/Assets/Scripts/GrapplingHook.cs
@@ -4,41 +4,52 @@ using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
-    public DistanceJoint2D distanceJoint;
-    public Camera mainCamera;
-    public LineRenderer lineRenderer;
-
+    readonly DistanceJoint2D distanceJoint;
+    readonly LineRenderer lineRenderer;
     Vector2 mousePos;
+
+    public float distance;
+    public LayerMask layerMaskRope;
 
     void Start()
     {
+        distanceJoint.gameObject.GetComponent<DistanceJoint2D>();
+        lineRenderer.gameObject.GetComponent<LineRenderer>();
+
+        lineRenderer.enabled = false;
         distanceJoint.enabled = false;
     }
 
     void Update()
     {
+        lineRenderer.SetPosition(0, transform.position);
+        mousePos = GetMousePos();
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            mousePos = GetMousePos();
-            lineRenderer.SetPosition(0, mousePos);
-            lineRenderer.SetPosition(1, transform.position);
-            distanceJoint.connectedAnchor = mousePos;
-            distanceJoint.enabled = true;
-            lineRenderer.enabled = true;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, mousePos, distance, layerMaskRope);
+
+            if(raycastHit2D.collider != null)
+            {
+                distanceJoint.enabled = true;
+                distanceJoint.connectedAnchor = raycastHit2D.point;
+
+                lineRenderer.enabled = true;
+                lineRenderer.SetPosition(1, raycastHit2D.point);
+            }
         }
         else if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             distanceJoint.enabled = false;
             lineRenderer.enabled = false;
         }
-
-        if (distanceJoint.enabled)
-            lineRenderer.SetPosition(1, transform.position);
-            
     }
 
-    Vector2 GetMousePos()
+    Vector3 GetMousePos()
     {
-        return mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseForwardPoint = mouseWorldPoint + (Camera.main.transform.forward * 10.0f);
+        Vector3 direction = mouseForwardPoint - transform.position;
+        return direction;
     }
 }
