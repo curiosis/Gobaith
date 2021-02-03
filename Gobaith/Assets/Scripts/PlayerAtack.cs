ecf8f18bde7793caf6426639587538f4daa4da72
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-02-03 12:07:09 +0100

    Update

--- Gobaith/Assets/Scripts/PlayerAtack.cs
+++ Gobaith/Assets/Scripts/PlayerAtack.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class PlayerAtack : MonoBehaviour
 {
     public GameObject atackParticleSystem;
+    public float damageRange;
+    float distance;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        GameObject enemy = GameObject.FindGameObjectWithTag("Wizard");
+
+        distance = Vector2.Distance(transform.position, enemy.transform.position);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             Instantiate(atackParticleSystem, transform.position, Quaternion.identity);
+            if (damageRange > distance)
+                Debug.Log("Frajer");
+        }
     }
 }
